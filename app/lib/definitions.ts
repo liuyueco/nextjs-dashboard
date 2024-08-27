2302f5e93b7049654413ebd8ced9516b2efa3f4c
yu <1831264870@qq.com>
2024-08-27 16:32:27 +0800

    fix

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,6 +2,14 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
+export type Invoice = {
+  id: string; // Will be created on the database
+  customer_id: string;
+  amount: number; // Stored in cents
+  status: 'pending' | 'paid';
+  date: string;
+};
+
 export type User = {
   id: string;
   name: string;
